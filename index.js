fdfbbf4341c5bba0688caeca6081b7122458a92a
PujaTiwa <pujat4582gamil.com>
2024-03-10 23:50:11 +0530

    Created responsive portfolio

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#text", {
+    strings: ["<i>Frontend Developer</i>", "<i>Backend Developer</i>", "<i>Web Developer</i>"],
+    typeSpeed:50,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+});
